c6785455049b8689f724450a0817b8af401912eb
zhirui <lizhirui0@gmail.com>
2026-05-11 15:35:56 +0800

    extracted data from nba_api as csv file

---
+++ backend/data.py
@@ -0,0 +1,16 @@
+from nba_api.stats.endpoints import shotchartdetail
+shots = shotchartdetail.ShotChartDetail(
+    team_id=0,
+    player_id=0,
+    season_type_all_star="Regular Season",
+    season_nullable="2025-26",
+    context_measure_simple="FGA",
+    timeout= 60
+)
+
+df = shots.get_data_frames()[0]
+
+#print("Shape:", df.shape)
+#print("\nColumns:\n", df.columns.tolist())
+
+df.to_csv('202526_shots.csv', index=False)
