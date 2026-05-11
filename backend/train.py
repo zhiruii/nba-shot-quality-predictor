999da054b7432933a9e90f4e11b80a745eded97e
zhirui <lizhirui0@gmail.com>
2026-05-11 17:46:04 +0800

    add one-hot, fitted log regression model

--- backend/train.py
+++ backend/train.py
@@ -1,9 +1,25 @@
 import pandas as pd
 from feature import featurize
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("202526_shots.csv")
 
 rows = df.to_dict(orient = 'records')
-features = [featurize(row) for row in rows]
+features_df = pd.DataFrame([featurize(row) for row in rows])
+
+cat_df = features_df[["SHOT_TYPE", "SHOT_ZONE_BASIC", "ACTION_TYPE"]]
+num_df = features_df[["SHOT_DISTANCE", "SHOT_ANGLE", "TIME_LEFT_IN_Q", "PERIOD"]]
+
+encoder = OneHotEncoder(sparse_output=False, handle_unknown="error")
+encoded = encoder.fit_transform(cat_df)
+
+# Convert num_df into numpy and stack horizontally to fit into model.
+X = np.hstack([num_df.to_numpy(), encoded])
+
+y = df["SHOT_MADE_FLAG"]
+
+model = LogisticRegression()
+model.fit(X, y)
