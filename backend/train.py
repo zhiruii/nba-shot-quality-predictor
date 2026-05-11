28781a358134dfd32e0cf9ff5eb263ab44564212
zhirui <lizhirui0@gmail.com>
2026-05-11 19:56:12 +0800

    test model ROC-AUC, score of 0.65. improve later

--- backend/train.py
+++ backend/train.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from feature import featurize
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 df = pd.read_csv("202526_shots.csv")
 
@@ -18,8 +18,15 @@ encoded = encoder.fit_transform(cat_df)
 
 # Convert num_df into numpy and stack horizontally to fit into model.
 X = np.hstack([num_df.to_numpy(), encoded])
+X = StandardScaler().fit_transform(X)
 
 y = df["SHOT_MADE_FLAG"]
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter = 1000)
 model.fit(X, y)
+
+cross_validation = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
+scores = cross_val_score(model, X, y, cv=cross_validation, scoring= "roc_auc")
+print(scores.round(3))
+print(scores.mean().round(3))
+print(scores.std().round(3))
