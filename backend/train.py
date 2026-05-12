360159ea524b3cd5111c896d3d451d8baa019b75
zhirui <lizhirui0@gmail.com>
2026-05-12 12:18:42 +0800

    add pipeline to prevent contamination of data when cross_validating model. no change in roc-auc.

--- backend/train.py
+++ backend/train.py
@@ -3,7 +3,8 @@ from feature import featurize
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-#from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.pipeline import Pipeline
 import pickle
 import json
 
@@ -19,13 +20,25 @@ encoder = OneHotEncoder(sparse_output=False, handle_unknown="error")
 encoded = encoder.fit_transform(cat_df)
 
 # Convert num_df into numpy and stack horizontally to fit into model.
-X = np.hstack([num_df.to_numpy(), encoded])
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+X_unscaled = np.hstack([num_df.to_numpy(), encoded])
 
 y = df["SHOT_MADE_FLAG"]
 
-model = LogisticRegression(max_iter = 1000)
+#pipeline made to prevent contamination of data during cross validation. not used in the final model training.
+pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("model", LogisticRegression(max_iter=1000))
+])
+
+#cross_validation = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
+#scores = cross_val_score(pipeline, X_unscaled, y, cv=cross_validation, scoring= "roc_auc")
+#print(scores.round(3))
+#print(scores.mean().round(3))
+#print(scores.std().round(3))
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X_unscaled)
+model = LogisticRegression(max_iter=1000)
 model.fit(X, y)
 
 #print(encoder.get_feature_names_out())
@@ -41,9 +54,3 @@ with open("scaler.pkl", "wb") as f2:
 num_cols = ["SHOT_DISTANCE", "SHOT_ANGLE", "TIME_LEFT_IN_Q", "PERIOD"]
 feature_names = num_cols + encoder.get_feature_names_out().tolist()
 json.dump(feature_names, open("feature_names.json", "w"))
-
-#cross_validation = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
-#scores = cross_val_score(model, X, y, cv=cross_validation, scoring= "roc_auc")
-#print(scores.round(3))
-#print(scores.mean().round(3))
-#print(scores.std().round(3))
