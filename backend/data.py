377c7e3a443edf652982714f0a425156395f6c54
zhirui <lizhirui0@gmail.com>
2026-06-10 22:06:47 +0800

    add player stats to match player FG% to the shots taken. additional features of FG to hopefully improve model.

--- backend/data.py
+++ backend/data.py
@@ -1,4 +1,5 @@
-from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.endpoints import shotchartdetail, leaguedashplayerstats
+
 shots = shotchartdetail.ShotChartDetail(
     team_id=0,
     player_id=0,
@@ -14,3 +15,14 @@ df = shots.get_data_frames()[0]
 #print("\nColumns:\n", df.columns.tolist())
 
 df.to_csv('202526_shots.csv', index=False)
+
+player_stats = leaguedashplayerstats.LeagueDashPlayerStats(
+    season="2025-26",
+    season_type_all_star="Regular Season",
+    timeout=60
+)
+
+player_df = player_stats.get_data_frames()[0]
+player_df = player_df[["PLAYER_ID", "FG_PCT", "FG3_PCT"]]
+
+player_df.to_csv('player_stats.csv', index=False)
