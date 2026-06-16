46c02aa1edd3c844d8baf8c40259ce42d113c17b
zhirui <lizhirui0@gmail.com>
2026-06-16 17:26:35 +0800

    merge player_stats into shots table

--- backend/data.py
+++ backend/data.py
@@ -14,17 +14,15 @@ shot_kwargs = dict(
 if date_from is not None:
     shot_kwargs["date_from_nullable"] = date_from.strftime("%m/%d/%Y")
 
-shots = shotchartdetail.ShotChartDetail(**shot_kwargs)
-df = shots.get_data_frames()[0]
-db.insert_shots(df.to_dict(orient="records"))
-
 player_stats = leaguedashplayerstats.LeagueDashPlayerStats(
     season="2025-26",
     season_type_all_star="Regular Season",
     timeout=60
 )
-
-player_df = player_stats.get_data_frames()[0]
-player_df = player_df[["PLAYER_ID", "PLAYER_NAME", "FG_PCT", "FG3_PCT"]]
-
+player_df = player_stats.get_data_frames()[0][["PLAYER_ID", "PLAYER_NAME", "FG_PCT", "FG3_PCT"]]
 db.upsert_player_stats(player_df.to_dict(orient="records"))
+
+shots = shotchartdetail.ShotChartDetail(**shot_kwargs)
+df = shots.get_data_frames()[0]
+df = df.merge(player_df[["PLAYER_ID", "FG_PCT", "FG3_PCT"]], on="PLAYER_ID", how="left")
+db.insert_shots(df.to_dict(orient="records"))
